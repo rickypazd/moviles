82b1ccc7495e0f1e886f255d2cadffa56a764a0d
Edson Gonzalo Mamani Veizaga <37853995+edsoncito@users.noreply.github.com>
2018-06-27 16:50:29 -0400

    sads

--- app/src/main/java/com/example/ricardopazdemiquel/moviles/cobranza.java
+++ app/src/main/java/com/example/ricardopazdemiquel/moviles/cobranza.java
@@ -3,7 +3,7 @@ package com.example.ricardopazdemiquel.moviles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Cobranza extends AppCompatActivity {
+public class cobranza extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
