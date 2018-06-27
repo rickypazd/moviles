69535eae1306980c014a2c90cf3b12747e8aefde
Ricardo Paz Demiquel <rickypazd@icloud.com>
2018-06-27 14:01:54 -0400

    llegaste a destino  cliente

---
+++ app/src/main/java/com/example/ricardopazdemiquel/moviles/finalizar_viajeCliente.java
@@ -0,0 +1,13 @@
+package com.example.ricardopazdemiquel.moviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class finalizar_viajeCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalizar_viajecliente);
+    }
+}
