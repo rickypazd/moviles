94adcdcf3978fd3daa0163c5f025699dd63c5248
Edson Gonzalo Mamani Veizaga <37853995+edsoncito@users.noreply.github.com>
2018-07-03 16:12:28 -0400

    add

---
+++ app/src/main/java/com/example/ricardopazdemiquel/moviles/Metodo_de_pagoCliente.java
@@ -0,0 +1,13 @@
+package com.example.ricardopazdemiquel.moviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Metodo_de_pagoCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodo_de_pagocliente);
+    }
+}
